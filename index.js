b960a18097570422643aabe18db4e0a92a889f84
Arda Barış Tonç <abt@Ardas-MacBook-Pro.local>
2020-12-20 14:18:06 +0300

    Soru ekleri için yeni regex eklenmesi

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   fix: function (str) {
-    return str.replace(/([^'])([td])([ae])([\s\n,.])/gm, '$1 d$3$4')
+    let questionFix = str.replace(/([^\s])(m(ı|i|u|ü))/gm, "$1 $2")
+    return questionFix.replace(/([^'])([td])([ae])([\s\n,.])/gm, "$1 d$3$4")
   }
 }
